692ea73a732e6387a15534a49154b9aced575291
Joël Fah <69576717+Joel-Fah@users.noreply.github.com>
2024-11-20 09:04:39 +0100

    feat: added folder for ui handling (#6)
    * feat: added project structure
    
    * feat: added db models and db utils functions
    
    * feat: added user model and integrated with db utility
    
    * feat: added logging utils and main entry point
    
    * feat: added ams sub menu after successful login
    
    * feat: added CRUD operations for User management
    
    * feat: added folder for ui handling

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 class User:
-    def __init__(self, username, email, password, role):
+    def __init__(self, id, username, email, password, role):
+        self.id = id
         self.username = username
         self.email = email
         self.password = password
